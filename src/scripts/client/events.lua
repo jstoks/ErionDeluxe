8e68fc6f0c26078e7b7a5146313c777bcfef57f8
Justin Scott <justin.scott@revelry.co>
2025-05-26 16:57:16 -0500

    Completes crafting events and sounds.

--- src/scripts/client/events.lua
+++ src/scripts/client/events.lua
@@ -246,6 +246,83 @@ erion.EventSystem:defineEventTable('game', {
       twistfail = ev('CraftTwistFail', 'Twist the willow shoot too forcefully and it breaks.', exactTrigger('You twist too forcefully and the willow shoot snaps and breaks!')),
       craftunpick = ev('CraftUnpick', 'Unpick cotton from a prayer rug.', msdpHandler('UNPICK')),
     },
+    foraging = {
+      forage = ev('Currently foraging', 'Foraging for something.', msdpHandler('FORAGE')),
+      berrybush = ev('ForageBerryBush', 'Examining a berry bush while foraging.', exactTrigger({
+        "You examine a berry growing from a bush.",
+        "You examine some colorful berries on a branch.",
+      })),
+      collectrare = ev("ForageCollectRare", "Collecting a rare item while foraging.", msdpHandler('COLLECT_RARE')),
+      collectwet = ev("ForageCollectWet", "Collecting something from the wetlands while foraging.", msdpHandler('COLLECT_WET')),
+      combgrass = ev("ForageCombGrass", "Comb through grass while foraging.", exactTrigger({
+        "You part some tall grass and comb the area.",
+        "You comb through some low grass."
+      })),
+      combsilt = ev("ForageCombSilt", "Combing through silt while foraging.", exactTrigger("You comb through the silt, seeking hidden treasures.")),
+      digsoil =  ev("ForageDigSoil", "Digging into the soil to unearth something while foraging.", regexTrigger({
+        "^You dig into the soil and unearth.*\\.$",
+        "^You dig into the soil and extract.*\\.$",
+      })),
+      feather = ev("Feather", "Feather of liberation from foraging.", msdpHandler('FEATHER')),
+      foundnothing = ev("ForageFoundNothing", "Found nothing useful while foraging.", exactTrigger("You didn't find anything useful.")),
+      spotgem = ev("ForageGem", "Find a gem while foraging.", regexTrigger({
+        "^You discover.*among a patch of stones\\!$",
+        "^You find.*gleaming at the bottom of a puddle\\!$"
+        "^You spot.*tucked inside the knothole of a tree\\!$"
+      })),
+      leafsnarl = ev("ForageLeafSnarl", "Clear away leaves while foraging and monster jumps out and snarls.", regexTrigger("^You clear away a pile of leaves and.*leaps forth with a snarl!$")),
+      liftbranch = ev("ForageLiftBranch", "Lifting a branch while foraging.", exactTrigger("You lift a low branch of a tree and examine the ground.")),
+      rockgrowl = ev("ForageRockGrowl", "Turn over a rock while foraging and monster jumps out and growls.", regexTrigger("^You push a rock to turn it over and.*wakes with a rumbling growl!$")),
+      siftmuck = ev("ForageSiftMuck", "Sifting through muck while foraging.", exactTrigger("You sift through the muck.")),
+      mudgrime = ev("ForageMudGrime", "Splashing through mud and grime while foraging.", exactTrigger("You splash through mud and grime, hoping to find loot.")),
+      peekknothole = ev("ForagePeekKnothole", "Peek in the knothole of a tree while foraging.", exactTrigger("You peek inside the knothole of a tree.")),
+      peekrock = ev("ForagePeekRock", "Peek under a rock while foraging.", exactTrigger("You peek under a rock.")),
+      pickbush = ev("ForagePickBush", "Picking something from a bush while foraging.", regexTrigger("^You pluck.*from the ground where it clings to a bit of moss\\.$")),
+      pickedover = ev("ForagePickedOver", "The area is picked over while foraging.", exactTrigger("The area looks picked over.")),
+      pluckbranch = ev("ForagePluckBranch", "Pluck something from a branch while foraging.", regexTrigger({
+        "^You pluck.*caught on a branch\\.$",
+        "^You pluck.*caught on a branch\\.$",
+      })),
+      rinsewater = ev("ForageRinseWater", "Rinsing water off something while foraging.", regexTrigger("^You rinse.*off in the water!$")),
+      rockturnover = ev("ForageRockTurnover", "Turning over a rock while foraging.", regexTrigger("^You turn over a rock and find.*in the dirt\\.$")),
+      rolllog = ev("ForageRollLog", "Rolling over a log while foraging.", exactTrigger("You roll over a log covered in moss.")),
+      rottenlog = ev("ForageRottenLog", "Pushing over a rotten log while foraging.", regexTrigger("^You nudge a rotten log aside with your toe and discover .*!$")),
+      spotrune = ev("ForageRune", "Find a rune while foraging.", regexTrigger({
+        "^You spot.*pulsing in a hollow tree stump!$",
+        "^You spot.*glowing at the base of a tree!$",
+        "^You clear away some leaves and discover.*underneath a gnarled tree root!$",
+      })),
+      scoopsand = ev("ForageSand", "Scoop up sand while foraging.", msdpHandler('FORAGE_SAND')),
+      scoopfloat = ev("ForageScoopFloat", "Scooping up an item before it floats away while foraging.", regexTrigger("^You scoop up.*before it floats on by!$")),
+      scoopmuck = ev("ForageScoopMuck", "Scooping muck while foraging.", exactTrigger("You scoop your hand into the muck and pull out.*!")),
+      scraperesin = ev("ForageScrapeResin", "Scrape resin off a tree while foraging.", exactTrigger("You scrape resin off of a tree!")),
+      scuttle = ev("ForageScuttle", "Push over a log while foraging and monster scuttles out.", regexTrigger("^You push over a log and.*scuttles out!$")),
+      forageseed = ev("ForageSeed", "Unearth a seed while foraging.", regexTrigger("^You scoop away a handful of dirt and unearth.*!$")),
+      muck = ev("ForageMuck", "Muck sound while foraging.", exactTrigger("Something brushes your hand and you yank it out, clutching a handful of muck.")),
+      toewetleaves = ev("ForageToeWetLeaves", "Toe at a pile of wet leaves while foraging.", exactTrigger("You toe at a pile of wet leaves.")),
+      treebranch = ev("ForageTreeBranch", "Loosen something from a tree branch while foraging.", regexTrigger("^You carefully loosen.*from a tree branch\\.$")),
+      dip = ev("ForageDip", "Dipping hands into water while foraging.", exactTrigger("You dip your hand into the water and hope it comes back out again.")),
+      dipfish = ev("ForageDipFish", "Dipping and fishing around in water while foraging.", exactTrigger("You dip your hands into the water, fishing for something near the bottom.")),
+      wateryank = ev("ForageWaterYank", "Yanking hand out of water while foraging.", regexTrigger("^Your hand brushes against something and you quickly yank out.*from the water!$")),
+      willow = ev("Willow", "Forage for willow shoots.", exactTrigger("You snap a willow shoot from the base of a willow tree.")),
+      wipegunk = ev("ForageWipeGunk", "Wiping gunk off your hands while foraging.", exactTrigger("You wipe gunk off your hand, inspecting it closely.")),
+      sift = ev("ForageSift", "Sifting through muck while foraging.", exactTrigger("You sift through a handful of muck and pebbles.")),
+      seed = ev("ForageSeed", "Notification when you forage a seed.", msdpHandler('SEED')),
+      unearth = ev("ForageUnearth", "Unearth or collect something while foraging.", msdpHandler('UNEARTH')),
+    },
+    gardening = {
+      gardendestroy = ev("GardenDestroy", "Destroy a seed or the entire garden.", regexTrigger({
+        "^You dispose of.*seed.*\\.$",
+        "^You unearth.*and dispose of it\\.$",
+      })),
+      fertilize = ev("GardenFertilize", "Fertilize a seed while gardening.", regexTrigger("^You fertilize.*with a pile of dung\\.$")),
+      harvest = ev("Harvest", "Harvest a seed while gardening.", regexTrigger("^You pluck.*from the center of the blossom!$")),
+      plantseed = ev("GardenPlant", "Plant a seed while gardening.", regexTrigger("^You tuck.*into.*\\.$")),
+      waterseed = ev("GardenWater", "Water a seed while gardening.", msdpHandler('WATER_GARDEN')),
+    },
+    leatherworking = {
+      tannincomplete = ev("TanninComplete", "Sound for skin turning to leather in a vat.", msdpHandler('TANNIN_COMPLETE')),
+    },
     mining = {
       debris = ev('Debris', 'Debris tumble over the minig site.', exactTrigger('Dirt and rock tumble over the cluster.')),
       cleardebris = ev('ClearDebris', 'Clear aray debris while mining.', exactTrigger("You clear away dirt and rock from the cluster and start hacking away.")),
@@ -260,7 +337,42 @@ erion.EventSystem:defineEventTable('game', {
       coal = ev("Coal", "Obtain cole while mining.", regexTrigger("^You extract .*coal!$")),
       salt = ev("Salt", "Obtain salt while mining.", regexTrigger("^You extract .*salt!$")),
       mininggem = ev("MiningGem", "Obtain a gem while mining.", msdpHandler('MINING_GEM')),
-    }
+    },
+    smelting = {
+      beginsmelting = ev("BeginSmelting", "Sound for beginning the smelting process.", msdpHandler('BEGIN_SMELTING')),
+      smeltcool = ev("SmeltingCooldown", "When ores or gems have melted and are cool enough to touch.", msdpHandler('SMELT_COOLDOWN')),
+      dross = ev("Dross", "Dross has accumulated during the smelting process.", regexTrigger("^A thick layer of dross has accumulated on the surface of .*\\.$")),
+      skimdross = ev("SkimDross", "Clear away dross during the smelting process.", beginTrigger("You skim the layer of dross off a ")),
+      flameschange = ev("FlamesChange", "Flames weaken during the smelting process.", regexTrigger("^The flames change from.*to.*\\.$")),
+      skimfail = ev("SkimFail", "Skim off too much dross.", exactTrigger("You skim off too much dross and destroy the liquid ores.")),
+      stoke = ev("Stoke", "Stoke the flames during the smelting process.", beginTrigger("You stoke the flames and ")),
+    },
+    woodworking = {
+      chop = ev("Chop", "Chop down a tree.", regexTrigger({
+        "^You swing.*and create the first notch in.*"
+        "^You swing the axe at.*\\.$"
+      })),
+      hammermetal = ev("CraftHammerMetal", "Hammering metal materials.", msdpHandler('HAMMER_METAL')),
+      hammerwood = ev("CraftHammerWood", "Hammering wood materials.", msdpHandler('HAMMER_WOOD')),
+      splitlog = ev("SplitLog", "Split a log into blocks of wood.", regexTrigger("^You split.*into.*of wood\\.$")),
+      treewedge = ev("TreeWedge", "Sound chopping a 45-degree wedge in a tree.", msdpHandler('TREE_WEDGE')),
+      treefall = ev("Treefall", "Tree falls over during the felling process", regexTrigger({
+        "^.*starts to fall away from you and lands with a CRASH!$",
+        "^.*starts to fall in your direction and knocks you down with bone-crushing force! OUCH!$",
+      })),
+      treepush = ev("Treepush", "Push over a tree during the felling process.", regexTrigger("^You push.*over and it falls to the ground with a CRASH!$")),
+    },
+    wool = {
+      spinwheel = ev('Spin', 'Spin yarn on a spinning wheel.', msdpHandler('SPINNING_WHEEL')),
+    },
+
+    crush = ev("Crush", "Crush the contents of a mortar with a pestle.", beginTrigger("You crush up")),
+    engrave = ev("Engrave", "Engrave your name into a crafted item.", regexTrigger("^You engrave.*with your name\\.$")),
+    imbue = ev("Imbue", "The craft imbue command.", regexTrigger("^You imbue.*with.*\\.$")),
+    polishgem = ev("PolishGem", "Polish a gem to a shine.", regexTrigger("^You polish.*to a shine\\.$")),
+    polishgemfail = ev("PolishGemFail", "Fail to polish a gem.", regexTrigger("^You attempt to polish.*and scratch the surface instead*\\.$")),
+    strikematch = ev("StrikeMatch", "Strike a match to light something.", beginTrigger("You strike a match and ")),
+    woven = ev("CraftWoven", "Finish weaving strands of magic together.", regexTrigger("^You finish weaving the strands together, creating.*\\.$")),
   }
 })
 
